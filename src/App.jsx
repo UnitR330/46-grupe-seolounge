871bfece6d8ecc0baf9712ea7b937f0743775391
UnitR330 <145355731+UnitR330@users.noreply.github.com>
2023-11-24 09:13:08 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,51 @@
+
+
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { DemoPage } from "./pages/Demo";
-import { HomePage } from "./pages/Home";
+import { ServicesPage } from "./pages/ServicesPage";
+import { HomePage } from "./pages/HomePage";
+import { NoPage } from './pages/NoPage';
+import { RegisterPage } from './pages/RegisterPage';
+import { TosPage } from './pages/TosPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index path='/' element={<HomePage />} />
-        <Route path='/demo' element={<DemoPage />} />
-        {/* <Route path='/register' element={<RegisterPage />} /> */}
-        {/* <Route path='*' element={<NoPage />} /> */}
+        <Route path='/services' element={<ServicesPage />} />
+        <Route path='/tos' element={<TosPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='*' element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+/*
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { ServicesPage } from "./pages/ServicesPage";
+import { HomePage } from "./pages/HomePage";
+import { NoPage } from './pages/NoPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path='/' element={<HomePage />} />
+        <Route path='/services' element={<ServicesPage />} />
+        {/* <Route path='/register' element={<RegisterPage />} /> }
+        <Route path='*' element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App; */
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import { BusinessGrowth } from "../sections/BusinessGrowth";
+import { Footer } from "../sections/Footer";
+import { Header } from "../sections/Header";
+import { Hero } from "../sections/Hero";
+import { HowCanWeHelp } from "../sections/HowCanWeHelp";
+import { Services } from "../sections/Services";
+
+export function HomePage() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Hero />
+                <Services limit={3} />
+                <BusinessGrowth />
+                <HowCanWeHelp />
+            </main>
+            <Footer />
+        </>
+    );
+}
